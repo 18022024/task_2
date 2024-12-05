e223e40e3caacb890577605c5484895f980c2ec9
SuperUser <volkov.serzh17@mail.ru>
2024-12-05 23:54:20 +1000

    task 3 commit

--- main.py
+++ main.py
@@ -1,54 +1,31 @@
-import io
-import sys
 import random
+import sys
 
-from PyQt6 import uic
 from PyQt6.QtCore import QPointF
-from PyQt6.QtGui import QPainter, QColor
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>Form</class>
- <widget class="QWidget" name="Form">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>400</width>
-    <height>500</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Form</string>
-  </property>
-  <widget class="QPushButton" name="pushButton">
-   <property name="geometry">
-    <rect>
-     <x>110</x>
-     <y>410</y>
-     <width>151</width>
-     <height>51</height>
-    </rect>
-   </property>
-   <property name="text">
-    <string>generate</string>
-   </property>
-  </widget>
- </widget>
- <resources/>
- <connections/>
-</ui>
-"""
+from PyQt6.QtGui import QColor, QPainter
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
 
 
-class Circles(QMainWindow):
+class Interface(QWidget):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 800, 800)
+        self.setWindowTitle('Git и случайные окружности')
+        self.but = QPushButton(self)
+        self.but.move(325, 600)
+        self.but.resize(200, 50)
+        self.but.setText('generate')
+
+
+class Circles(Interface):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
         self.do_paint = False
-        self.pushButton.clicked.connect(self.paint)
+        self.but.clicked.connect(self.push)
 
     def paintEvent(self, event):
         if self.do_paint:
@@ -58,18 +35,27 @@ class Circles(QMainWindow):
             qp.end()
         self.do_paint = False
 
-    def paint(self):
+    def push(self):
         self.do_paint = True
         self.update()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        c1 = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+        c2 = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+        c3 = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+        c4 = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
         xy1 = random.randint(0, 100)
         xy2 = random.randint(0, 100)
         xy3 = random.randint(0, 100)
-        qp.drawEllipse(QPointF(100, 100), xy1, xy1)
-        qp.drawEllipse(QPointF(200, 200), xy2, xy2)
-        qp.drawEllipse(QPointF(300, 300), xy3, xy3)
+        xy4 = random.randint(0, 100)
+        qp.setBrush(QColor(c1[0], c1[1], c1[2]))
+        qp.drawEllipse(QPointF(100, 300), xy1, xy1)
+        qp.setBrush(QColor(c2[0], c2[1], c2[2]))
+        qp.drawEllipse(QPointF(300, 300), xy2, xy2)
+        qp.setBrush(QColor(c3[0], c3[1], c3[2]))
+        qp.drawEllipse(QPointF(500, 300), xy3, xy3)
+        qp.setBrush(QColor(c4[0], c4[1], c4[2]))
+        qp.drawEllipse(QPointF(700, 300), xy4, xy4)
 
 
 if __name__ == '__main__':
